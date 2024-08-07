04a36faecd00aebf33c6bfad3bf569771326ab1c
arelyx <39994376+arelyx@users.noreply.github.com>
2024-08-07 01:15:48 -0700

    fighting with flexbox

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,7 +4,35 @@ function App() {
 
   return (
     <>
-      <h1>hi</h1>
+    <div id="container">
+      <div id="header">
+        <h1>header</h1>
+      </div>
+      <div id="content">
+        <div id="sidebar">
+          <div id="chats_container">
+            <div id="chats">
+              <p>chat 1</p>
+              <p>chat 2</p>
+              <p>chat 3</p>
+              <p>chat 4</p>
+              <p>chat 5</p>
+              <p>chat 6</p>
+              <p>chat 7</p>
+              <p>chat 8</p>
+              <p>chat 9</p>
+              <p>chat 10</p>
+            </div>
+          </div>
+          <div id="account">
+            <h3>account</h3>
+          </div>
+        </div>
+        <div id="chatbox">
+          <h2>chatbox</h2>
+        </div>
+      </div>
+    </div>
     </>
   )
 }
